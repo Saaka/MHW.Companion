d044ac908d65ccf5d60ac93dad7fc434bd7907e9
Michal Sakwa <sakwa.michal@gmail.com>
2018-03-20 22:08:04 +0100

    Added project to store data models.

---
+++ MHW.Companion.Model/User/AppUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace MHW.Companion.Model.User
+{
+    public class AppUser : IdentityUser<int>
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public long? FacebookId { get; set; }
+    }
+}
